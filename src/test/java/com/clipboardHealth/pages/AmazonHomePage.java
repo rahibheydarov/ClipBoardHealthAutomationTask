284fdd108005f50239f24a090bc877d1fde1ef00
Rahib <heydarov.rahib@gmail.com>
2022-07-24 14:32:01 -0400

    Finalize = / About This Item Section Print

--- src/test/java/com/clipboardHealth/pages/AmazonHomePage.java
+++ src/test/java/com/clipboardHealth/pages/AmazonHomePage.java
@@ -5,9 +5,12 @@ import com.clipboardHealth.customUtils.ConfigurationReader;
 import com.clipboardHealth.customUtils.DriverUtil;
 import javafx.scene.layout.BackgroundRepeat;
 import org.openqa.selenium.By;
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -48,6 +51,9 @@ public class AmazonHomePage {
     @FindBy (xpath = "//*[text()=' About this item ']")
     WebElement aboutThisItem;
 
+    @FindBy (xpath = "//*[@id=\"feature-bullets\"]/ul")
+    WebElement aboutThisSectionText;
+
 
     public AmazonHomePage(WebDriver driver){
         this.driver = driver;
@@ -84,9 +90,6 @@ public class AmazonHomePage {
 
         television.click();
 
-        JavascriptExecutor js = (JavascriptExecutor) DriverUtil.getDriver();
-        js.executeScript("window.scrollBy(0, 1750)");
-
         WebDriverWait wait = new WebDriverWait(driver,10);
 
         WebElement word1 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"s-refinements\"]/div[21]/ul/li[3]/span/a/span")));
@@ -103,19 +106,17 @@ public class AmazonHomePage {
         priceRangeHighToLow.click();
         highToLowPriceFilter.click();
 
-//        List<WebElement> allTvOptions = driver.findElements(By.xpath("xpath is dynamic but I can solve this Java option in a longer time"));
-//        BrowserUtils.sleep(1);
-//        allTvOptions.get(1).click();
-
         searchResultNo2.click();
 
-        BrowserUtils.sleep(3);
+        BrowserUtils.sleep(2);
 
-        WebDriverWait wait = new WebDriverWait(driver,10);
+        List<WebElement> allAboutThisSectionLines = driver.findElements(By.xpath("//*[@id=\"feature-bullets\"]/ul"));
 
-        WebElement word1 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[text()=' About this item ']")));
-        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", word1);
+        for (WebElement each : allAboutThisSectionLines) {
 
+            System.out.println(each.getText());
+
+        }
 
         return this;
     }
