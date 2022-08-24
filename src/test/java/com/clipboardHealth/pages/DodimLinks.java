873171977e5d40e2b2d9442fb9897cd989793f5b
Rahib <heydarov.rahib@gmail.com>
2022-08-24 14:01:53 -0400

    Canim Dodisim Programlasdirma

--- src/test/java/com/clipboardHealth/pages/DodimLinks.java
+++ src/test/java/com/clipboardHealth/pages/DodimLinks.java
@@ -19,16 +19,13 @@ public class DodimLinks {
 
     public static void main(String[] args) throws IOException {
 
-        //I have placed an excel file 'Test.xlsx' in my D Driver
         FileInputStream fis = new FileInputStream("src/undone_links.xlsx");
         XSSFWorkbook workbook = new XSSFWorkbook(fis);
         XSSFSheet sheet = workbook.getSheetAt(0);
-        //I have added test data in the cell A1 as "SoftwareTestingMaterial.com"
-        //Cell A1 = row 0 and column 0. It reads first row as 0 and Column A as 0.
+
         Row row = sheet.getRow(0);
         Cell cell = row.getCell(0);
-//        System.out.println(cell);
-//        System.out.println(sheet.getRow(2).getCell(1));
+
 
         List<String> allLinks = new ArrayList<>();
 
@@ -42,7 +39,8 @@ public class DodimLinks {
 
         for (int j = 1; j < 10000; j++) {
             DriverUtil.getDriver().get(allLinks.get(j));
-            BrowserUtils.sleep(1);
+            System.out.println("Hazirki link nomresi " +j);
+            BrowserUtils.sleep(2);
         }
 
 
